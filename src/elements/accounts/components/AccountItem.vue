1ae181f78f321627c4896a0c75c89cb1e47fd169
DitarMainWorkstation <ditarrogozhnikov@gmail.com>
2022-04-21 12:37:59 +0300

    Added: AccountItem Updated: AccluntList logic

---
+++ src/elements/accounts/components/AccountItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <tr class="rowData">
+        <td><p>{{account.name}}</p></td>
+        <td><p>{{account.account}}</p></td>
+        <td><p>{{account.mail}}</p></td>
+        <td><p>{{account.password}}</p></td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        props:{
+            account: {
+                type: Object,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style>
+    .rowData {
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
+        text-align: center;
+    }
+
+
+    th {
+        background-color: #42b983;
+        color: rgba(255, 255, 255, 0.66);
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    td {
+        background-color: #f9f9f9;
+    }
+
+    th,
+    td {
+        padding: 10px 20px;
+    }
+
+</style>
--- src/elements/accounts/components/AccountList.vue
+++ src/elements/accounts/components/AccountList.vue
@@ -3,26 +3,17 @@
         <table>
             <thead>
             <tr>
-                <th class="columnTitle" v-for="name in columNames" :key="name.id">
-                    <p>{{name.body}}</p>
+                <th class="columnTitle" v-for="column in columNames" :key="column.id">
+                    <p>{{column.body}}</p>
                 </th>
             </tr>
             </thead>
             <tbody>
-            <tr class="rowData" v-for="row in accounts" :key="row.id">
-                <td>
-                    <p>{{row.name}}</p>
-                </td>
-                <td>
-                    <p>{{row.account}}</p>
-                </td>
-                <td>
-                    <p>{{row.mail}}</p>
-                </td>
-                <td>
-                    <p>{{row.password}}</p>
-                </td>
-            </tr>
+            <account-item
+                    v-for="account in accounts"
+                    :key="account.id"
+                    :account="account"
+                />
             </tbody>
         </table>
     </div>
@@ -30,7 +21,9 @@
 </template>
 
 <script>
+    import AccountItem from "@/elements/accounts/components/AccountItem";
     export default {
+        components: {AccountItem},
         data() {
             return {
                 columNames: [
@@ -63,38 +56,16 @@
         border-radius: 3px;
         background-color: #fff;
         width: 100%;
-    }
 
-    th {
-        background-color: #42b983;
-        color: rgba(255, 255, 255, 0.66);
-        cursor: pointer;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-    }
-
-    td {
-        background-color: #f9f9f9;
-    }
-
-    th,
-    td {
-        min-width: 300px;        padding: 10px 20px;
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
     }
 
     .columnTitle {
         padding: 15px;
         border: 2px solid teal;
         margin-top: 15px;
-    }
-
-    .rowData {
-        padding: 15px;
-        border: 2px solid teal;
-        margin-top: 15px;
-        text-align: center;
     }
 
 </style>
