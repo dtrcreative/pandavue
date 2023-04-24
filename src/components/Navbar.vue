9cea145f34acb0e40c7cfcf8c7ed25b60db317c1
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-04-24 10:55:01 +0300

    Added: auth-service and auth-header services Refact: logIn button now shows log in dialog

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="navbar">
-    <div @click="$router.push('/')">i113_Panda_main</div>
+    <div @click="$router.push('/')"><strong>i113_Panda_main</strong></div>
     <div class="navbar__btns">
-      <panda-button style="margin-right: 10px" @click="$router.push('/login')">LogIn</panda-button>
+      <panda-button style="margin-right: 10px" @click="showLoginDialog">LogIn</panda-button>
       <panda-button style="margin-right: 10px" @click="$router.push('/panda')">Panda</panda-button>
     </div>
+      <panda-dialog v-model:show="logInDialogVisible">
+        <auth-vue></auth-vue>
+      </panda-dialog>
   </div>
 </template>
 
 <script>
 import PandaButton from "@/components/UI/PButton";
+import PandaDialog from "@/components/UI/PDialog";
+import AuthVue from "@/components/AuthVue";
 export default {
   name: "MainVue",
-  components: {PandaButton}
+  components: {AuthVue, PandaDialog, PandaButton},
+
+  data() {
+    return {
+      logInDialogVisible: false,
+    }
+  },
+  methods:{
+    showLoginDialog(){
+      this.logInDialogVisible = !this.logInDialogVisible;
+    }
+  }
 }
 </script>
 export default {
+
 }
 <style scoped>
 
