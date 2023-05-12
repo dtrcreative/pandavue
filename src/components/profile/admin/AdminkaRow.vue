dfad4018f6d7309ce778062040656f4424656c12
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-12 10:36:39 +0300

    Added: files Admin table and row

---
+++ src/components/profile/admin/AdminkaRow.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrapper"
+       v-on:mouseover="mouseover"
+       v-on:mouseleave="mouseleave"
+       :class="{hovering: isHovering}"
+  >
+    <p-cell
+        :value="user.username"
+    ></p-cell>
+    <p-cell
+        :value="user.email"
+    ></p-cell>
+    <p-cell
+        :value="user.role"
+    ></p-cell>
+    <p-cell
+        :value="user.status"
+    ></p-cell>
+
+    <panda-button
+        @click="$emit('remove', unit)"
+    ><font-awesome-icon icon="trash" />
+    </panda-button>
+  </div>
+</template>
+
+<script>
+import PandaButton from "@/components/UI/PButton";
+export default {
+  name: "AdminkaRow",
+  components: {PandaButton},
+  data() {
+    return {
+      isHovering: false,
+    }
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    }
+  },
+  methods: {
+    mouseover: function () {
+      this.isHovering = true
+    },
+    mouseleave: function () {
+      this.isHovering = false
+    },
+    setNotify() {
+      console.log("notify")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 5fr 5fr 5fr 1fr 1fr 1fr;
+  grid-template-rows: repeat(1, 1fr);
+}
+
+</style>
---
+++ src/components/profile/admin/AdminkaTable.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="scroll">
+    <div class="wrapper">
+      <div class="columnTitle" v-for="column in columNames" :key="column.id">
+        <p>{{ column.body }}</p>
+      </div>
+    </div>
+    <div>
+      <transition-group name="unit-list">
+        <adminka-row
+            v-for="unit in users"
+            :key="unit.id"
+            :unit="unit"
+        ></adminka-row>
+      </transition-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminkaRow from "@/components/profile/admin/AdminkaRow";
+
+export default {
+  name: "AdminkaTable",
+  components: {AdminkaRow},
+  data() {
+    return {
+      columNames: [
+        {id: 1, body: 'UserName'},
+        {id: 2, body: 'Email'},
+        {id: 3, body: 'Role'},
+        {id: 4, body: 'Status'},
+        {id: 5, body: 'Del'},
+      ],
+      checked: true,
+    }
+  },
+  props: {
+    users: {
+      type: Array,
+      request: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+.columnTitle {
+  max-height: 40px;
+  text-align: center;
+  padding: 5px;
+  border: 2px solid teal;
+  border-radius: 8px;
+  background-color: aliceblue;
+}
+
+.scroll{
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  max-height: 800px;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 5fr 5fr 5fr 50px 50px 50px;
+  grid-template-rows: repeat(1, 1fr);
+}
+.unit-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.unit-list-enter-active,
+.unit-list-leave-active {
+  transition: all 0.4s ease;
+}
+.unit-list-enter-from,
+.unit-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.unit-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
